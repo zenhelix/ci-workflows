f9f6fc4478fad5c246766374065bb126d2bae5dc
dmitriimedakin <dm.medakin.online@gmail.com>
2026-04-14 00:09:00 +0200

    chore: add kotlinx-serialization-json dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("io.github.typesafegithub:github-workflows-kt:3.7.0")
     implementation("com.charleskorn.kaml:kaml:0.104.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
 
     // JIT action bindings
     implementation("actions:checkout:v6")
