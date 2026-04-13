63c96327c2068b3b011fa3b48f20bb772d657222
dmitriimedakin <dm.medakin.online@gmail.com>
2026-04-14 00:14:32 +0200

    refactor: migrate GradleCreateTagAdapter to typed builder

--- src/main/kotlin/workflows/adapters/tag/GradleCreateTag.kt
+++ src/main/kotlin/workflows/adapters/tag/GradleCreateTag.kt
@@ -1,10 +1,8 @@
 package workflows.adapters.tag
 
-import config.APP_SECRETS
 import config.DEFAULT_JAVA_VERSION
 import config.DEFAULT_RELEASE_BRANCHES
 import config.SetupTool
-import config.passthrough
 import dsl.AdapterWorkflow
 import dsl.CreateTagWorkflow
 import dsl.ReusableWorkflowJobDef
@@ -19,17 +17,17 @@ object GradleCreateTagAdapter : AdapterWorkflow("gradle-create-tag.yml") {
     val tagPrefix = input("tag-prefix", description = "Prefix for the tag", default = "")
     val releaseBranches = input("release-branches", description = "Comma-separated branch patterns for releases", default = DEFAULT_RELEASE_BRANCHES)
 
-    init { secrets(APP_SECRETS) }
+    override fun createJobBuilder() = CreateTagWorkflow.JobBuilder()
 
     override fun jobs(): List<ReusableWorkflowJobDef> = listOf(
-        reusableJob(id = "create-tag", uses = CreateTagWorkflow) {
-            CreateTagWorkflow.setupAction(SetupTool.Gradle.id)
-            CreateTagWorkflow.setupParams(SetupTool.Gradle.toParamsJson(javaVersion.ref))
-            CreateTagWorkflow.checkCommand(gradleCommand.ref)
-            CreateTagWorkflow.defaultBump(defaultBump.ref)
-            CreateTagWorkflow.tagPrefix(tagPrefix.ref)
-            CreateTagWorkflow.releaseBranches(releaseBranches.ref)
-            secrets(APP_SECRETS.passthrough())
+        reusableJob<CreateTagWorkflow.JobBuilder>(id = "create-tag", uses = CreateTagWorkflow) {
+            setupAction(SetupTool.Gradle.id)
+            setupParams(SetupTool.Gradle.toParamsJson(javaVersion.ref))
+            checkCommand(gradleCommand.ref)
+            defaultBump(this@GradleCreateTagAdapter.defaultBump.ref)
+            tagPrefix(this@GradleCreateTagAdapter.tagPrefix.ref)
+            releaseBranches(this@GradleCreateTagAdapter.releaseBranches.ref)
+            passthroughAllSecrets()
         },
     )
 }
