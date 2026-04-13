167d34a7a5243a15e2075d7f83c91836d4fa925d
dmitriimedakin <dm.medakin.online@gmail.com>
2026-04-14 00:14:40 +0200

    refactor: migrate GoManualCreateTagAdapter to typed builder

--- src/main/kotlin/workflows/adapters/tag/GoManualCreateTag.kt
+++ src/main/kotlin/workflows/adapters/tag/GoManualCreateTag.kt
@@ -1,9 +1,7 @@
 package workflows.adapters.tag
 
-import config.APP_SECRETS
 import config.DEFAULT_GO_VERSION
 import config.SetupTool
-import config.passthrough
 import dsl.AdapterWorkflow
 import dsl.ManualCreateTagWorkflow
 import dsl.ReusableWorkflowJobDef
@@ -17,16 +15,16 @@ object GoManualCreateTagAdapter : AdapterWorkflow("go-manual-create-tag.yml") {
     val checkCommand = input("check-command", description = "Go validation command", default = "make test")
     val tagPrefix = input("tag-prefix", description = "Prefix for the tag", default = "v")
 
-    init { secrets(APP_SECRETS) }
+    override fun createJobBuilder() = ManualCreateTagWorkflow.JobBuilder()
 
     override fun jobs(): List<ReusableWorkflowJobDef> = listOf(
-        reusableJob(id = "manual-tag", uses = ManualCreateTagWorkflow) {
-            ManualCreateTagWorkflow.tagVersion(tagVersion.ref)
-            ManualCreateTagWorkflow.tagPrefix(tagPrefix.ref)
-            ManualCreateTagWorkflow.setupAction(SetupTool.Go.id)
-            ManualCreateTagWorkflow.setupParams(SetupTool.Go.toParamsJson(goVersion.ref))
-            ManualCreateTagWorkflow.checkCommand(checkCommand.ref)
-            secrets(APP_SECRETS.passthrough())
+        reusableJob<ManualCreateTagWorkflow.JobBuilder>(id = "manual-tag", uses = ManualCreateTagWorkflow) {
+            tagVersion(this@GoManualCreateTagAdapter.tagVersion.ref)
+            tagPrefix(this@GoManualCreateTagAdapter.tagPrefix.ref)
+            setupAction(SetupTool.Go.id)
+            setupParams(SetupTool.Go.toParamsJson(goVersion.ref))
+            checkCommand(this@GoManualCreateTagAdapter.checkCommand.ref)
+            passthroughAllSecrets()
         },
     )
 }
